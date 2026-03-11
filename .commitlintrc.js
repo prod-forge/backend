b721bae24f86fe3219fdb50f27f922f70c3dddbb
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2026-03-11 11:35:41 +0100

    fix: fix release cycle

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'release'],
-    ],
+    'type-enum': [2, 'always', ['ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style']],
   },
 };
